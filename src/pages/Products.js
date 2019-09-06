8c8a5f75698a9516421f3405fba0ce0ed9247929
mochen <mochen0505@hotmail.com>
2019-09-06 18:23:40 +0800

    minor

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,56 +1,54 @@
-import React, { Component } from 'react';
+import React, {useContext} from 'react';
 import Header from '../components/Header';
 import Table from 'antd/es/table';
 import Button from 'antd/es/button';
 import ShopContext from '../context/shop-context';
 
-class Products extends Component {
-    static contextType = ShopContext;
+const Products = props => {
+    const context = useContext(ShopContext);
 
-    handleClick = (id) => {
-        this.context.addProductToCart(id)
+    const handleClick = (id) => {
+        context.addProductToCart(id)
     };
 
-    render() {
-        const columns = [
-            {
-                title: 'ID',
-                dataIndex: 'id',
-                key: 'id',
-            },
-            {
-                title: 'Name',
-                dataIndex: 'name',
-                key: 'name',
-            },
-            {
-                title: 'Price',
-                dataIndex: 'price',
-                key: 'price',
-            },
-            {
-                title: 'Operation',
-                dataIndex: 'operation',
-                key: 'operation',
-                render: (text, rows) => (
-                    <Button type='primary' onClick={() => this.handleClick(rows.id)}>Add</Button>
-                )
-            },
-        ];
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Price',
+            dataIndex: 'price',
+            key: 'price',
+        },
+        {
+            title: 'Operation',
+            dataIndex: 'operation',
+            key: 'operation',
+            render: (text, rows) => (
+                <Button type='primary' onClick={() => handleClick(rows.id)}>Add</Button>
+            )
+        },
+    ];
 
-        return (
-            <React.Fragment>
-                <Header/>
-                <Table
-                    style={{margin: 50}}
-                    columns={columns}
-                    dataSource={this.context.products}
-                    rowKey='id'
-                />
-            </React.Fragment>
-        )
-    }
-}
+    return (
+        <React.Fragment>
+            <Header/>
+            <Table
+                style={{margin: 50}}
+                columns={columns}
+                dataSource={context.products}
+                rowKey='id'
+            />
+        </React.Fragment>
+    )
+};
 
 export default Products
 
